9f09fb296cbc889878aae243cf55f1ae367bb414
ianmcateer <ian-mcateer55@hotmail.com>
2018-04-04 13:46:58 +1000

    created one to one relationship between cart and user

--- db/migrate/20180404015714_add_columns_to_users.rb
+++ db/migrate/20180404015714_add_columns_to_users.rb
@@ -2,7 +2,7 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :username, :string
+    add_column :users, :username, :string, unique: true
     add_column :users, :role, :string
     add_column :users, :disabled, :boolean
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404030859) do
+ActiveRecord::Schema.define(version: 20180404031650) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_carts_on_product_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
