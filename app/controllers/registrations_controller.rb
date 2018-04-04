9ccad40770a60bc994e38d4aba71ec6e60935c70
chullman <christopherhullman@gmail.com>
2018-04-04 19:11:52 +1000

    bug fixes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,32 @@ class RegistrationsController < Devise::RegistrationsController
   
   # Extending the Devise Reistrations Controller to permit the new username, first_name, and last_name fields
 
+  # Need to override the original create action so that we can hard code user.role = "normal" and user.disabled = false
+  def create
+    build_resource(sign_up_params)
+
+    resource.role = "normal"
+    resource.disabled = false
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
   private
 
   def sign_up_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :carts
+  resources :products
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  root to: "products#index"
+
   get 'admins/index'
 
   get 'users/index'
@@ -7,9 +13,5 @@ Rails.application.routes.draw do
 
   get ':controller(/:action)' 
 
-  resources :carts
-  resources :products
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
